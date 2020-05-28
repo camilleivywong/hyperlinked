46c004fa00b63425a5abf4b40c418b69ba0f6caa
camilleivywong <66044667+camilleivywong@users.noreply.github.com>
2020-05-27 18:07:53 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,22 @@
+// var LINKS = document.querySelectorAll('#links a');
+
+//     for (var i = 0; i < LINKS.length; i++) {
+//         // I think here is where I set function for loop
+//     }
+
+function displayLink(){
+   var LINKS = document.getElementById("link");
+
+   if (LINKS.style.display === "none") {
+    LINKS.style.display = "block";
+   } else {
+    LINKS.style.display = "none";
+   }
+}
+
+  // for (var i = 0; i < LINKS.length; i++){
+
+
+
+
+
